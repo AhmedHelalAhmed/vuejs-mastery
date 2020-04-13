f05b94a90ccf5aa3d7d610864d3ba0d78415c8ba
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-04-14 01:58:17 +0200

    finalize

--- beginner/intro-to-vue-js/vue-instance/main.js
+++ beginner/intro-to-vue-js/vue-instance/main.js
@@ -1,1 +1,17 @@
-var product = "Socks";
+var app = new Vue({
+  el: "#app",//<================= element
+  data: {
+    product: "Socks",
+    description: "This is description",
+  },
+}); // root or the vue app
+
+/*
+{
+
+}
+^
+^
+^
+option objects
+*/
